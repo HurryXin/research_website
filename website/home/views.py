e8dd8ca4ae17940bdbac380f8817cba5debd0732
hurryxin <ruixinhurry@gmail.com>
2018-04-15 14:24:08 +0800

    add home app in INSTALL_APP

--- website/home/views.py
+++ website/home/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    context = {}
+    return render(request, 'home/index.html', context)
