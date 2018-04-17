08ca7a5a916978ab2dffc6337c2654e6bb93e10f
hurryxin <ruixinhurry@gmail.com>
2018-04-17 18:28:25 +0800

    add table trade_id etc

--- website/caesar/models.py
+++ website/caesar/models.py
@@ -4,5 +4,6 @@ from django.db import models
 
 class BrainModule(models.Model):
     trade_id = models.PositiveIntegerField(default=0)
+    module = models.PositiveSmallIntegerField(default=0)
     predict = models.NullBooleanField()
     result = models.NullBooleanField()
