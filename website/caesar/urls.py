6e981eafc453177a89d22a088950b1e5ddd44783
hurryxin <ruixinhurry@gmail.com>
2018-04-21 14:17:44 +0800

    test ajax function

--- website/caesar/urls.py
+++ website/caesar/urls.py
@@ -7,4 +7,5 @@ from . import views
 app_name = 'caesar'
 urlpatterns = [
     url(r'^', views.caesar, name='caesar'),
+    url(r'^caesar', views.caesar, name='caesar'),
 ]
--- website/caesar/views.py
+++ website/caesar/views.py
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.template import loader
+# import json
 
 
+# @require_http_method(["GET"])
+# @csrf_token
 def caesar(request):
-    context = {}
+    if request.is_ajax():
+        value = request.GET.get('value')
+        print value
+        print type(value)
+        test = int(value)
+        print test
+        print isinstance(test,int)
+
+
+
+    image1 = "Ae"
+    context = {'image': image1,}
     return render(request, 'caesar/showcaesar.html', context)
